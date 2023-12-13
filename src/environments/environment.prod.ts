bc7ef8206df7d7724ddf961fcb42eaca020ea15c
Cucu Toma Daniel <cucutomadaniel@gmail.com>
2023-12-13 16:21:55 +0200

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  githubToken: 'test_token',
+  githubToken: '',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  githubToken: 'test_token',
+  githubToken: '',
 };
