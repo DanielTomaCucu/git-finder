06735b497f1d5dd1d8176311c158bdb0659b3edc
Cucu Toma Daniel <cucutomadaniel@gmail.com>
2023-12-13 15:53:08 +0200

    test

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -6,13 +6,14 @@ import { HttpLink } from 'apollo-angular/http';
 import { ApolloLink } from '@apollo/client/core';
 import { environment } from '../environments/environment';
 
+
 const uri = 'https://api.github.com/graphql';
 
 export function createApollo(httpLink: HttpLink) {
   const authLink = new ApolloLink((operation, forward) => {
     operation.setContext({
       headers: {
-        Authorization: `Bearer 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`,
+        Authorization: `Bearer ${environment.githubToken}`,
       },
     });
     return forward(operation);
